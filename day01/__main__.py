c78a33802c3dadcb33896b140f6b13782a164862
Joe Atkins <atreides322@mac.com>
2023-12-03 01:08:56 -0600

    Day 01 Part 2

--- day01/__main__.py
+++ day01/__main__.py
@@ -1,7 +1,37 @@
 import os.path
 from sys import argv
+import regex
+
+translation = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+}
+
+pattern = '(one|two|three|four|five|six|seven|eight|nine|[0-9])'
+
+
+def numbers(line):
+    first = translation[regex.findall(pattern, line)[0]]
+    last = translation[regex.findall(pattern, line, flags=regex.REVERSE)[0]]
+    return int(first + last)
+
 
 with open(os.path.join(os.path.dirname(__file__), argv[1])) as fp:
-    numbers = [[digit for digit in line if digit.isdigit()] for line in fp.readlines()]
-    cal_value = [int(line[0] + line[-1]) for line in numbers]
-    print(sum(cal_value))
+    values = [numbers(line) for line in fp.readlines()]
+    print(sum(values))
