6416ae8d9ed684f9cb8c0d35fcfa7d339a0c3e9b
Joe Atkins <atreides322@mac.com>
2023-12-04 20:23:04 -0600

    Day 02 Part 2

--- day02/__main__.py
+++ day02/__main__.py
@@ -1,3 +1,6 @@
+import functools
+import itertools
+import operator
 import os.path
 from sys import argv
 
@@ -8,19 +11,39 @@ MAX = {
 }
 
 
-def valid(cubes):
-    parts = cubes.split(' ')
-    return int(parts[0]) <= MAX[parts[1]]
+def parse_color(color):
+    parts = color.split(' ')
+    return parts[1], int(parts[0])
 
 
 def parse(line):
     label, games = line.strip().split(': ', 2)
+    number = int(label.split(' ')[1])
     games = [game.split(', ') for game in games.split('; ')]
-    possible = all(all([valid(color) for color in game]) for game in games)
+    games = [dict(parse_color(color) for color in game) for game in games]
+    return number, games
 
-    return int(label.split(' ')[1]) if possible else 0
+
+def valid_draw(color, count):
+    return count <= MAX[color]
 
 
-with open(os.path.join(os.path.dirname(__file__), argv[1])) as fp:
-    values = [parse(line) for line in fp.readlines()]
-    print(sum(values))
+def valid_game(rounds):
+    return all(all(valid_draw(color, count) for color, count in round_.items()) for round_ in rounds)
+
+
+def min_required(rounds):
+    flattened = [(k, v) for round_ in rounds for k, v in round_.items()]
+    grouped = itertools.groupby(sorted(flattened), lambda x: x[0])
+    return {k: max([num for (_, num) in v]) for k, v in grouped}
+
+
+with (open(os.path.join(os.path.dirname(__file__), argv[1])) as fp):
+    games = [parse(line) for line in fp.readlines()]
+    values = [(num, valid_game(rounds)) for num, rounds in games]
+    total = sum(num for num, valid in values if valid)
+    print('Part 1:', total)
+
+    required = [min_required(rounds) for _, rounds in games]
+    powers = [functools.reduce(operator.mul, game.values()) for game in required]
+    print('Part 2:', sum(powers))
